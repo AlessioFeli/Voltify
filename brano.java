01229c67f5e74787ba2deba921a3bc4252672ade
AlessioFeli <93184410+AlessioFeli@users.noreply.github.com>
2022-01-18 09:23:07 +0100

    Create brano.java

---
+++ brano.java
@@ -0,0 +1,16 @@
+package com.example.voltify;
+
+import java.util.Date;
+
+public class brano {
+    private String titolo;
+    private String autore;
+    private int durata;
+    private Date datacreazione;
+
+
+    public brano(String titolo)
+    {
+        this.titolo=titolo;
+    }
+}
