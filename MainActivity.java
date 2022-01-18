3fc085b7b66088944c843e4172b02d5b96740962
AlessioFeli <93184410+AlessioFeli@users.noreply.github.com>
2022-01-18 09:39:35 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity<gestionebrani> extends AppCompatActivity {
-    gestionebrani gb;
+    Gestionebrani gb;
     Button btnInserisci;
     EditText txtTitolo;
+    Spinner spnGenere;
+    String[] elencoGeneri={"rap","punk"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,12 @@ public class MainActivity<gestionebrani> extends AppCompatActivity {
         gb= new gestionebrani();
         txtTitolo=findViewById(R.id.titolo);
         btnInserisci=findViewById(R.id.btnInserisci);
+        spnGenere = (Spinner)findViewById(R.id.spnGenere);
+
+        ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, elencoGeneri);
+
+        spnGenere.setAdapter(spGen);
+
         gb= new Gestionebrani();
         btnInserisci.setOnClickListener(new View.OnClickListener() {
             @Override
