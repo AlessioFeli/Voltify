c0e079220d40a20b3a53a2c4dd828bb8d2b8f919
AlessioFeli <93184410+AlessioFeli@users.noreply.github.com>
2022-01-18 09:23:35 +0100

    Create Gestionebrani.java

---
+++ Gestionebrani.java
@@ -0,0 +1,24 @@
+package com.example.voltify;
+
+import java.util.ArrayList;
+
+public class gestorebrani {
+    private String visualizzaTesto(String titolo) {
+        String testo = "";
+        return testo;
+        public class Gestionebrani {
+            ListaBrani =new ArrayList<Brano>();
+        }
+        public void addBrano (String titolo)
+        {
+            Brano br = new Brano(titolo);
+            listaBrani.add(br);
+        }
+    }
+
+
+    public void visualizzaBrani() {
+
+    }
+}
+
