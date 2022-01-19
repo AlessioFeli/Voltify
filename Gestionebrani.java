93bbbae0158ce4972860b0443117456f1d4e742a
AlessioFeli <93184410+AlessioFeli@users.noreply.github.com>
2022-01-19 21:09:32 +0100

    Update Gestionebrani.java

--- Gestionebrani.java
+++ Gestionebrani.java
@@ -1,57 +1,30 @@
 package com.example.voltify;
 
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnInserisci;
-    Button btnVisualizza;
-    EditText txtTitolo;
-    Spinner spnGenere;
-    String[] elencoGeneri={"rap","punk"};
-    Gestorebrani gb;
+public class Gestorebrani extends AppCompatActivity {
+    ListView Listabrani;
+    ArrayList<Brano> Listabrani;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnInserisci=findViewById(R.id.btnInserisci);
-        txtTitolo=findViewById(R.id.titolo);
-        spnGenere = (Spinner)findViewById(R.id.spnGenere);
-
-        ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, elencoGeneri);
-        spnGenere.setAdapter(spGen);
-        gb= new Gestorebrani();
-        btnInserisci.setOnClickListener(new View.OnClickListener() {
-            new View.OnClickListener(){
-                public void onClick(View view) {
-
-
-                    gb.addBrano(txtTitolo.getText().toString());
-                 }
-            }
-        });
-
-        btnVisualizza.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onclick(View v)
-                    gb.visualizza()
-                    Intent i = new Intent(MainActivity.this, Gestorebrani.class);
-
-                    startActivity(i)
-
-
-                }
-        );
-
-
-
+        setContentView(R.layout.Gestorebrani);
+        Listabrani=findViewById(R.id.Listabrani);
     }
-}
+    public void addBrano(String titolo){
+        Brano br =new Brano(titolo);
+        Listabrani.add(br);
+    }
+    public void visualizza(){
+        Listabrani.setAdapter((ListAdapter) Listabrani);
+    }
+    }
+
+
