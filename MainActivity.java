7baefd8fa97dfb4256c04dfb39ce46c045de3f53
AlessioFeli <93184410+AlessioFeli@users.noreply.github.com>
2022-01-19 21:10:05 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,45 +2,54 @@ package com.example.voltify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-public class MainActivity<gestionebrani> extends AppCompatActivity {
-    Gestionebrani gb;
+public class MainActivity extends AppCompatActivity {
     Button btnInserisci;
+    Button btnVisualizza;
     EditText txtTitolo;
     Spinner spnGenere;
     String[] elencoGeneri={"rap","punk"};
-
+    Gestorebrani gb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gb= new gestionebrani();
-        txtTitolo=findViewById(R.id.titolo);
         btnInserisci=findViewById(R.id.btnInserisci);
+        txtTitolo=findViewById(R.id.titolo);
         spnGenere = (Spinner)findViewById(R.id.spnGenere);
 
         ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, elencoGeneri);
-
         spnGenere.setAdapter(spGen);
-
-        gb= new Gestionebrani();
+        gb= new Gestorebrani();
         btnInserisci.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
+            new View.OnClickListener(){
+                public void onClick(View view) {
 
-                gb.addBrano(txtTitolo.getText().toString());
 
+                    gb.addBrano(txtTitolo.getText().toString());
+                 }
             }
         });
+
+        btnVisualizza.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onclick(View v)
+                    gb.visualizza()
+                    Intent i = new Intent(MainActivity.this, Gestorebrani.class);
+
+                    startActivity(i)
+
+
+                }
+        );
 
 
 
